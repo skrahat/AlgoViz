2d4dd4d8958ea7d5e8cd25ebe35b39b1301f0187
asaduzzaman rahat <skrahat@asaduzzamans-MacBook-Air.local>
2021-12-15 11:00:44 -0500

    Header: updating

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,52 +1,116 @@
-import { AppBar, makeStyles, Toolbar } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export default function Header() {
-    // const useStyles = makeStyles(() => ({
-    //     container: {
-    //         backgroundColor: '#400CCC'
-    //     }
-    // }));
-    // const classes = useStyles();
-    //const navigate = useNavigate();
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    //data for links to pages from dashboard
-    const headerData = [
-        {
-            label: 'Home',
-            href: '/home'
-        },
-        {
-            label: 'Evaluate',
-            href: '/evaluate'
-        },
-        {
-            label: 'Info',
-            href: '/info'
-        }
-    ];
-    //buttons to be used as navigator
-    const menuButtons = () => {
-        return headerData.map(({ label, href }) => {
-            return (
-                <button
-                    onClick={() => {
-                        // navigate(`${href}`);
-                    }}
-                >
-                    {label}
-                </button>
-            );
-        });
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
     };
 
-    const dashBoardHeader = () => {
-        return <Toolbar>Dashboard {menuButtons()}</Toolbar>;
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
     };
 
     return (
-        <header>
-            <AppBar>{dashBoardHeader()}</AppBar>
-        </header>
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                    >
+                        DashBoard
+                    </Typography>
+
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'flex', md: 'none' }
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            color="inherit"
+                        ></IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left'
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left'
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' }
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem
+                                    key={page}
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Typography textAlign="center">
+                                        {page}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'flex', md: 'none' }
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'none', md: 'flex' }
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
