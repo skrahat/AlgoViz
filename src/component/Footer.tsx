3b6480ce5d083dd7b7cb59929cd55b3982bcc6e5
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2023-07-08 01:51:07 -0400

    fix: updated footer and mainPage UI

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,29 +1,43 @@
 import React from 'react';
+import { Box, Typography, Link } from '@mui/material';
+import { colours } from '../styling/colours';
 
 const Footer = () => {
     const email = 'asaduzzaman.rahat@mail.mcgill.ca';
     const website = 'https://www.asadrahat.ca';
 
     return (
-        <footer
-            style={{
+        <Box
+            sx={{
+                backgroundColor: colours.secondary,
                 padding: '1rem',
                 textAlign: 'center',
-                marginTop: '2rem',
-                position: 'fixed',
-                bottom: '0',
-                width: '100%'
+                color: colours.primary
             }}
         >
-            <p>
-                <a href={website} target="_blank" rel="noopener noreferrer">
-                    asadrahat.ca
-                </a>
+            <Typography variant="body2" color="inherit">
+                <Link
+                    href={email}
+                    variant="caption"
+                    color="inherit"
+                    underline="none"
+                >
+                    Contact me
+                </Link>
                 <br />
-                Contact me! <a href={`mailto:${email}`}>{email}</a>
-                <br />
-            </p>
-        </footer>
+            </Typography>
+            <Typography variant="caption" color="inherit" mt={1}>
+                <Link
+                    href={website}
+                    variant="caption"
+                    color="inherit"
+                    underline="none"
+                >
+                    &copy; {new Date().getFullYear()} Asad Rahat. All rights
+                    reserved.
+                </Link>
+            </Typography>
+        </Box>
     );
 };
 
