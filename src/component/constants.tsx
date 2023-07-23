f3d68f24673b52ab2a92b27c7fa30c77a10dafe4
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2023-07-23 17:36:41 -0400

    feat: separate constant file

---
+++ src/component/constants.tsx
@@ -0,0 +1,29 @@
+import { createTheme } from '@mui/material';
+import { colours } from '../styling/colours';
+
+export const algorithmList = ['bubble', 'insertion'];
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+export const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: '4rem'
+        }
+    }
+};
+// Define the MUI theme
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: colours.primary
+        },
+        secondary: {
+            main: colours.secondary
+        },
+        text: {
+            primary: colours.accent,
+            secondary: colours.primary
+        }
+    }
+});
