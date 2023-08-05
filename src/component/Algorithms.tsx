d3b4716c487e8c33cb42e378c31a8b22819121be
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2023-08-05 19:20:22 -0400

    added new merge sort algorithm code

--- src/component/Algorithms.tsx
+++ src/component/Algorithms.tsx
@@ -122,6 +122,76 @@ export const InsertionSort = async (
     dispatch(sortInProgressAction(false));
     dispatch(sortedAction(true));
 };
+export const mergeSort = async (
+    result: any[],
+    signal: AbortSignal,
+    dispatch: any,
+    graphNumber: number
+) => {
+    const newArray = [...result];
+
+    // The helper function to merge two arrays
+    const merge = async (left: any[], right: any[]) => {
+        let resultArray = [],
+            leftIndex = 0,
+            rightIndex = 0;
+
+        // We will concatenate values into the resultArray in order
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex].value < right[rightIndex].value) {
+                resultArray.push(left[leftIndex]);
+                leftIndex++; // move left array cursor
+            } else {
+                resultArray.push(right[rightIndex]);
+                rightIndex++; // move right array cursor
+            }
+        }
+
+        // We need to concat to the resultArray because there will be one element remaining from either left OR the right
+        return resultArray
+            .concat(left.slice(leftIndex))
+            .concat(right.slice(rightIndex));
+    };
+
+    const sort = async (array: any[]) => {
+        if (array.length === 1) {
+            // return once we hit an array with a single item
+            return array;
+        }
+
+        const middle = Math.floor(array.length / 2); // get the middle item of the array
+        const left = array.slice(0, middle); // items on the left side
+        const right = array.slice(middle); // items on the right side
+
+        // Check if the abort signal is triggered
+        if (signal.aborted) {
+            dispatch(sortNumbersMergeAction(array, graphNumber));
+            return array;
+        }
+
+        const sortedLeft = await sort(left);
+        const sortedRight = await sort(right);
+        const mergedArray = await merge(sortedLeft, sortedRight);
+
+        dispatch(sortNumbersMergeAction(mergedArray, graphNumber));
+        dispatch(iterationsCompletedAction(false, 0));
+        await timer(middle);
+
+        return mergedArray;
+    };
+
+    const sortedArray = await sort(newArray);
+
+    // Set the color of all elements to green to indicate the sorting is complete
+    const finalSortedArray = sortedArray.map((item) => {
+        return { ...item, color: colours.success };
+    });
+
+    dispatch(sortNumbersMergeAction(finalSortedArray, graphNumber));
+
+    dispatch(sortInProgressAction(false));
+    dispatch(sortedAction(true));
+};
 
 // delay timer function
 async function timer(ms: number) {
