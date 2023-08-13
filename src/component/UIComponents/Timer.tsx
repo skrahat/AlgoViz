65d5b746b8ea99f6bf11b2ada8539a5c74736894
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2023-08-13 00:18:47 -0400

    time component issue fixed

--- src/component/UIComponents/Timer.tsx
+++ src/component/UIComponents/Timer.tsx
@@ -11,11 +11,16 @@ const Timer: React.FC<TimerProps> = ({
     showMilliseconds = false
 }) => {
     const [timeElapsed, setTimeElapsed] = useState(0);
+    const [previousInProgress, setPreviousInProgress] = useState(false);
 
     useEffect(() => {
         let intervalId: NodeJS.Timeout | null = null;
 
         if (inProgress) {
+            if (!previousInProgress) {
+                setTimeElapsed(0); // Reset the timer when inProgress changes from false to true
+            }
+
             intervalId = setInterval(() => {
                 setTimeElapsed((prevTime) => prevTime + 100); // Increment by 100ms
             }, 100); // Update every 100ms
@@ -26,6 +31,10 @@ const Timer: React.FC<TimerProps> = ({
                 clearInterval(intervalId);
             }
         };
+    }, [inProgress, previousInProgress]);
+
+    useEffect(() => {
+        setPreviousInProgress(inProgress); // Update previousInProgress when inProgress changes
     }, [inProgress]);
 
     const formatTime = (time: number) => {
