a5de9788ad12130491a7f06704b8594c4ea3761a
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2023-08-17 21:33:51 -0400

    update Dashboard.tsx

--- src/page/Dashboard.tsx
+++ src/page/Dashboard.tsx
@@ -1,24 +1,9 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-    AppBar,
-    Box,
-    Toolbar,
-    Typography,
-    Container,
-    Slider,
-    Paper,
-    FormControlLabel,
-    SelectChangeEvent,
-    OutlinedInput,
-    FormControl,
-    Select,
-    MenuItem
-} from '@mui/material';
+import { Box, Typography, Container, SelectChangeEvent } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from '@mui/material/styles';
-import CustomButton from '../component/UIComponents/CustomButton';
 import {
     generateNumbersAction,
     sortInProgressAction,
@@ -31,13 +16,10 @@ import {
 } from '../component/algorithms/Algorithms';
 import Footer from '../component/UIComponents/Footer';
 import BarGraph from '../component/graphComponent/BarGraph';
-import Switch from '@mui/material/Switch';
 import { colours } from '../styling/colours';
 import FactCard from '../component/UIComponents/FactCard';
-import { MenuProps, algorithmList, theme } from '../component/constants';
+import { theme } from '../component/constants';
 import { SortingFunctions } from './Dashboard.type';
-import CustomAlert from '../component/UIComponents/Alert';
-import Timer from '../component/UIComponents/Timer';
 import AppBarSection from './AppBarSection';
 
 export default function Dashboard(): JSX.Element {
